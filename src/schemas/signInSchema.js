28d0d74b7bc6f09c69a2a37967686c396da614ca
Guga <contato.gustavobarbosa@outlook.com>
2022-06-20 15:46:08 -0300

    Preparando revisão de código

--- src/schemas/signInSchema.js
+++
@@ -1,8 +0,0 @@
-import joi from 'joi';
-
-const signInSchema = joi.object().keys({
-    email: joi.string().email().required(),
-    password: joi.string().required(),
-});
-
-export default signInSchema;
--- src/schemas/urlSchema.js
+++
@@ -1,7 +0,0 @@
-import joi from 'joi';
-
-const urlSchema = joi.object().keys({
-    url: joi.string().uri().required(),
-});
-
-export default urlSchema;
